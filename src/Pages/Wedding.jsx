a8f3da1c1d025a502e20e9c8801f1edd554a3d70
ShantoMRozario <shantoxrozario@gmail.com>
2025-02-27 11:33:25 +0600

    change img path

--- src/Pages/Wedding.jsx
+++ src/Pages/Wedding.jsx
@@ -8,7 +8,7 @@ const Wedding = () => {
 
                 <Link to='/RupakKona' className="w-[300px] h-[400px] bg-slate-700 relative group/item">
                     <img className="w-full h-full object-cover group-hover/item:opacity-50 group-hover/item:scale-95 transition duration-300 " 
-                        src="/src/images/image.png" alt="" />
+                        src="../images/image.png" alt="" />
                     <h3 className=" font-bold text-[20px] absolute bottom-[50%] left-[50%] translate-x-[-50%] translate-y-[50%] invisible group-hover/item:visible transition duration-300">Rupak & Kona</h3>
                 </Link>
                 <Link to='/RupakKona' className="w-[300px] h-[400px] bg-slate-700 relative group/item">
